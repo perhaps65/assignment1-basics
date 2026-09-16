66c0ece018da3a463f51b06f689e753181f5e9e7
perhaps65 <1920678846@qq.com>
2026-09-16 23:23:32 +0800

    implement rope

--- cs336_basics/ch03_transformer/s05_rope.py
+++ cs336_basics/ch03_transformer/s05_rope.py
@@ -3,11 +3,29 @@
 from __future__ import annotations
 
 from torch import Tensor, nn
+import torch
 
 
 class RotaryPositionalEmbedding(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None) -> None:
-        raise NotImplementedError
+        super().__init__()
+        positions = torch.arange(max_seq_len, device=device, dtype=torch.float32)
+        dims = torch.arange(0, d_k, 2, device=device, dtype=torch.float32,)
+        freq = theta ** (-dims / d_k)
+        positions = positions[:,None]
+        freq = freq[None,:]
+        angle = freq * positions
+        self.register_buffer("cos", torch.cos(angle), persistent=False)
+        self.register_buffer("sin", torch.sin(angle), persistent=False)
 
     def forward(self, x: Tensor, token_positions: Tensor) -> Tensor:
-        raise NotImplementedError
+        even = x[..., 0::2]
+        odd = x[..., 1::2]
+        cos = self.cos[token_positions]
+        sin = self.sin[token_positions]
+        rotated_even = even * cos + odd * sin
+        rotated_odd = -even * sin + odd * cos
+        rotate = torch.stack([rotated_even, rotated_odd], dim=-1).flatten(-2)
+        
+        return rotate
+
