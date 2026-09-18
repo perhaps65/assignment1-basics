1f27d937a193b9d53d850fbe07768df668d7cbfd
perhaps65 <1920678846@qq.com>
2026-09-18 16:42:43 +0800

    交叉熵 完成

--- cs336_basics/ch04_training/s01_cross_entropy.py
+++ cs336_basics/ch04_training/s01_cross_entropy.py
@@ -3,7 +3,16 @@
 from __future__ import annotations
 
 from torch import Tensor
+import torch
 
 
 def cross_entropy(inputs: Tensor, targets: Tensor) -> Tensor:
-    raise NotImplementedError
+    # -log(exp(correct - max) / sum(exp(logits - max)))
+    # log(sum(exp(logits - max))) - correct + max
+    # [B,T] - [B,T]
+    correct = torch.gather(inputs, dim=-1, index=targets.unsqueeze(-1)).squeeze(dim=-1) 
+    # [B,T,1]->[B,T]
+    max_value = inputs.max(dim=-1,keepdim=True).values
+    log_sum_exp = torch.log(torch.sum(torch.exp(inputs - max_value), dim=-1, keepdim=False))
+    loss = log_sum_exp - correct + max_value.squeeze(dim=-1)
+    return loss.mean()
--- cs336_basics/ch04_training/s02_sgd_experiments.py
+++ cs336_basics/ch04_training/s02_sgd_experiments.py
@@ -1,10 +1,62 @@
-"""4.2 SGD 实验。仅接口骨架，待自行实现。"""
+"""4.2 SGD 实验。"""
 
 from __future__ import annotations
 
+import math
+from collections.abc import Callable
+from typing import Optional
+
+import torch
+
+
+class SGD(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3):
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+
+        defaults = {"lr": lr}
+        super().__init__(params, defaults)
+
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+
+        for group in self.param_groups:
+            lr = group["lr"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                state = self.state[p]
+                t = state.get("t", 0)
+                grad = p.grad.data
+
+                p.data -= lr / math.sqrt(t + 1) * grad
+
+                state["t"] = t + 1
+
+        return loss
+
 
 def main() -> None:
-    raise NotImplementedError
+    for lr in [1e1, 1e2, 1e3]:
+        print(f"\n===== lr = {lr} =====")
+        torch.manual_seed(0)
+        weights = torch.nn.Parameter(
+            5 * torch.randn((10, 10))
+        )
+
+        opt = SGD([weights], lr=lr)
+
+        for t in range(10):
+            opt.zero_grad()
+
+            loss = (weights ** 2).mean()
+
+            print(f"step {t}: loss = {loss.item()}")
+
+            loss.backward()
+            opt.step()
 
 
 if __name__ == "__main__":
